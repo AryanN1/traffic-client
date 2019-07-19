f5ad53b192acdf26048c42afdf2fa7cc7979a915
Aryan Naimi <aryannaimi@gmail.com>
2019-07-19 12:00:51 -0700

    Re-added heatmap back

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,7 +10,7 @@ class Map extends React.Component {
     this.state = {
       incidents :[],
       geoJsonData: {},
-      heatmapVisible: false
+      heatmapVisible: true
     }
   }
   componentDidMount() {
